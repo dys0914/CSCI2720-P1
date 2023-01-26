296f5a872f71a1cfd65ae04438ca2d1a1f5ac580
dys0914 <123596634+dys0914@users.noreply.github.com>
2023-01-25 19:18:16 -0500

    Create ItemType.java

---
+++ ItemType.java
@@ -0,0 +1,22 @@
+public class ItemType {
+    private int value;
+    public ItemType() {
+        value = 0;
+    }
+    public ItemType(int num) {
+        value = num;
+    }
+    public int compareTo(ItemType item) {
+        int ret = 0;
+        if(item.getValue() > value) {
+            ret = -1;
+        }
+        else if(item.getValue() > value) {
+            ret = 1;
+        }
+        return ret;
+    }
+    public int getValue() {
+        return value;
+    }
+}
