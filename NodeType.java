395e605cf0ddd460df475f47bbe798c4174767e3
dys0914 <123596634+dys0914@users.noreply.github.com>
2023-01-25 19:18:36 -0500

    Create NodeType.java

---
+++ NodeType.java
@@ -0,0 +1,15 @@
+public class NodeType {
+    public ItemType info;
+    public NodeType next;
+    public NodeType() {
+        info = null;
+        next = null;
+    }
+    public NodeType(ItemType i) {
+        info = i;
+    }
+    public NodeType(ItemType i, NodeType n) {
+        info = i;
+        next = n;
+    }
+}
